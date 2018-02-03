0a6ef8fce5ee6e2efbb08ff0916f9733a3970e0f
Azariah Aeroborn <izzionsona@gmail.com>
2018-02-03 09:25:01 -0500

    initial work to display stats

--- ff14parser-viewmodel.js
+++ ff14parser-viewmodel.js
@@ -28,11 +28,71 @@ var parserViewModel = function() {
         ,stats: ["Strength", "Direct Hit", "Critical Hit", "Determination", "Skill Speed", "Vitality", "Tenacity"]
         ,statproperties: ["strength", "directhit", "criticalhit", "determination", "skillspeed", "vitality", "tenacity"]
         ,weapon: [
-            {name: "Nightsteel Sword", level: 350, damage: 101, strength: 225, vitality: 241, determination: 204, skillspeed: 143}
-            ,{name: "Byakko's Stone Sword", level: 355, damage: 101, strength: 231, vitality: 249, skillspeed: 209, criticalhit: 146}
-            ,{name: "Homura", level: 360, damage: 102, strength: 236, vitality: 256, criticalhit: 213, tenacity: 149}
-            ,{name: "Homura Kai", level: 370, damage: 104, strength: 248, vitality: 271, criticalhit: 222, tenacity: 156}
-            ,{name: "Diamond Sword", level: 375, damage: 104, strength: 254, vitality: 279, determination: 227, criticalhit: 159}
+                {
+                    name: "Nightsteel Sword"
+                   ,level: 350
+                   ,damage: 101
+                   ,stats: [{name: "strength", value: 225}
+                        , {name: "directhit", value: null}
+                        , {name: "criticalhit", value: null}
+                        , {name: "determination", value: 204}
+                        , {name: "skillspeed", value: 143}
+                        , {name: "vitality", value: 241}
+                        , {name: "tenacity", value: null}
+                    ]
+                }
+               ,{
+                    name: "Byakko's Stone Sword"
+                   ,level: 355
+                   ,damage: 101
+                   ,stats: [{name: "strength", value: 231}
+                        , {name: "directhit", value: null}
+                        , {name: "criticalhit", value: 146}
+                        , {name: "determination", value: null}
+                        , {name: "skillspeed", value: 209}
+                        , {name: "vitality", value: 249}
+                        , {name: "tenacity", value: null}
+                    ]
+                }
+                ,{
+                    name: "Homura"
+                    ,level: 360
+                    ,damage: 102
+                    ,stats: [{name: "strength", value: 236}
+                        , {name: "directhit", value: null}
+                        , {name: "criticalhit", value: 213}
+                        , {name: "determination", value: null}
+                        , {name: "skillspeed", value: null}
+                        , {name: "vitality", value: 256}
+                        , {name: "tenacity", value: 149}
+                    ]
+                }
+                ,{
+                    name: "Homura Kai"
+                    ,level: 370
+                    ,damage: 104
+                    ,stats: [{name: "strength", value: 248}
+                        , {name: "directhit", value: null}
+                        , {name: "criticalhit", value: 222}
+                        , {name: "determination", value: null}
+                        , {name: "skillspeed", value: null}
+                        , {name: "vitality", value: 271}
+                        , {name: "tenacity", value: 156}
+                    ]
+                }
+                ,{
+                    name: "Diamond Sword"
+                    ,level: 375
+                    ,damage: 104
+                    ,stats: [{name: "strength", value: 254}
+                        , {name: "directhit", value: null}
+                        , {name: "criticalhit", value: 159}
+                        , {name: "determination", value: 227}
+                        , {name: "skillspeed", value: null}
+                        , {name: "vitality", value: 279}
+                        , {name: "tenacity", value: null}
+                    ]
+                }
         ]
         }
     ]);
@@ -43,6 +103,25 @@ var parserViewModel = function() {
 
         return filtered
     });
+
+    ko.bindingHandlers.displayStats = {
+        init: function(element, valueAccessor, allBindings) {
+            var itemStats = valueAccessor();
+            var itemStatsUnwrapped = ko.unwrap(itemstats);
+            var statsArray = allBindings.get('statsarray');
+
+            var child = ko.virtualElements.firstChild(elem),
+                childElems
+            ko.utils.arrayForEach(statsArray, function(stat){
+                output.push(itemStatsUnwrapped[stat]);
+            });
+
+            return output;
+        },
+        update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+
+        }
+    }
 };
 
 ko.applyBindings(new parserViewModel());
