3dc2a75085be929dafc996a121550a18a687cdbc
Azariah Aeroborn <izzionsona@gmail.com>
2018-04-02 20:50:22 -0400

    Progress toward collecting skill usage information

--- fflogs-deepdive/lib/viewmodel.js
+++ fflogs-deepdive/lib/viewmodel.js
@@ -174,11 +174,128 @@ var parserViewModel = function(){
                return f.id === fightdata.selectedFriendly();
            });
            if ( selected.length > 0 ) {
-               if ( classParser.class === selected[0].type ) {
+               var selectedFriendly = selected[0];
+               if ( classParser.class === selectedFriendly.type ) {
                    // Loaded parser matches the class of the selected friendly
+                   classParser.skills.forEach(function(curSkill){
+                       curSkill.usages = [];
+                       var usageList = selectedFriendly.skills[curSkill];
+                       var curUsage = null;
+                       while ( usage = usageList.shift() ) {
+                           if ( usage.type === "cast" ) {
+                               // Cast type usages indicate start of a new skill usage, all damage or buff/debuff info for previous one is complete
+                               // Push previous usage onto the stack of skill usages (curUsage will be null for the first occurrence of this skill, nothing to push onto stack)
+                               if ( curUsage !== null ) { curSkill.usages.push(curUsage); }
+                               curUsage = usage;
+                           } else {
+                               // The first occurrence of a skill might occur without a "cast" event, due to pre-casting before the pull
+                               // If so, synthesize a cast event from the skill information
+                               if ( curUsage === null ) {
+                                   curUsage = {
+                                       ability: usage.ability,
+                                       sourceID: usage.sourceID,
+                                       sourceIsFriendly: usage.sourceIsFriendly,
+                                       targetID: usage.targetID,
+                                       targetIsFriendly: usage.targetIsFriendly,
+                                       timestamp: usage.timestamp,
+                                       type: "cast"
+                                   };
+                                   if ( usage.targetIsFriendly === "false" ) {
+                                       curUsage.targetInstance = usage.targetInstance;
+                                   }
+                               }
+                               switch ( usage.type ) {
+                                   case "damage":
+                                       if ( usage.hasOwnProperty("tick") ) {
+                                           // damage of type "tick" is simulated DOT damage
+                                       } else {
+                                           // direct damage from use of a skill
+                                           curUsage.damage = {
+                                               amount: usage.amount,
+                                               absorbed: usage.absorbed,
+                                               debugMultiplier: usage.debugMultiplier,
+                                               hitType: usage.hitType,
+                                               sourceResources: usage.sourceResources,
+                                               targetResources: usage.targetResources,
+                                               timestamp: usage.timestamp
+                                           }
+                                       }
+                                       break;
+                                   case "applydebuff":
+                                       if ( !curUsage.hasOwnProperty("debuffs") ) {
+                                           curUsage.debuffs = [];
+                                       }
+                                       var debuffed = curUsage.debuffs.filter(function(obj){
+                                           return obj.targetID === usage.targetID;
+                                       });
+                                       if ( debuffed.length > 0 ) {
+                                           debuffed[0].targetInstances.push(usage.targetInstance);
+                                       } else {
+                                           curUsage.debuffs.push({
+                                               targetID: usage.targetID,
+                                               targetInstances: [usage.targetInstance],
+                                               starttime: usage.timestamp
+                                           })
+                                       }
+                                       break;
+                                   case "removedebuff":
+                                       if ( !curUsage.hasOwnProperty("debuffs") ) {
+                                           console.log("removedebuff event occurred outside of a cast event");
+                                       } else {
+                                           var debuffed = curUsage.debuffs.filter(function(obj){
+                                               return obj.targetID === usage.targetID;
+                                           });
+                                           if ( debuffed.length > 0 ) {
+                                               debuffed[0].endtime = usage.timestamp;
+                                           } else {
+                                               console.log("removedebuff event occurred without a matching target for the debuff")
+                                           }
+                                       }
+                                       break;
+                                   case "applybuff":
+                                       if ( !curUsage.hasOwnProperty("buffs") ) {
+                                           curUsage.buffs = [];
+                                       }
+                                       var buffed = curUsage.buffs.filter(function(obj){
+                                           return obj.targetID === usage.targetID;
+                                       });
+                                       if ( buffed.length > 0 ) {
+                                           buffed[0].targetInstances.push(usage.targetInstance);
+                                       } else {
+                                           curUsage.buffs.push({
+                                               targetID: usage.targetID,
+                                               targetInstances: [usage.targetInstance],
+                                               starttime: usage.timestamp
+                                           })
+                                       }
+                                       break;
+                                   case "removebuff":
+                                       if ( !curUsage.hasOwnProperty("buffs") ) {
+                                           console.log("removebuff event occurred outside of a cast event");
+                                       } else {
+                                           var buffed = curUsage.buffs.filter(function(obj){
+                                               return obj.targetID === usage.targetID;
+                                           });
+                                           if ( buffed.length > 0 ) {
+                                               buffed[0].endtime = usage.timestamp;
+                                           } else {
+                                               console.log("removebuff event occurred without a matching target for the buff")
+                                           }
+                                       }
+                                       break;
+                                   case "refreshdebuff":
+                                       // TODO: implement handling for refreshdebuff
+                                       break;
+                                   default:
+                                       console.log("unhandled event of type " + usage.type);
+                                       break;
+                               }
+                           }
+                       }
+                   });
                    console.log(classParser);
-                   console.log(selected[0].skills);
-                   return selected[0].name;
+                   console.log(selectedFriendly.skills);
+                   return selectedFriendly.name;
                } else {
                    return "Loading class information..."
                }
